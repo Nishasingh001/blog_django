c9b19f497dbdfb5e1bf7af1ddcdd71a48d587097
Nishasingh001 <152494067+Nishasingh001@users.noreply.github.com>
2023-12-03 15:49:27 +0530

    Update settings.py

--- app_blog/settings.py
+++ app_blog/settings.py
@@ -124,3 +124,8 @@ STATIC_URL = "static/"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 STATICFILES_DIRS = [BASE_DIR, "static"]
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
